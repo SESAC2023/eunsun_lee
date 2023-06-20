f51fc57a0588f67bca7ebfaede371830c24b6b1e
eunsun111 <136873704+eunsun111@users.noreply.github.com>
2023-06-20 22:24:10 +0900

    Update 7. 주사위_2480.py

--- baekjoon/2. 조건문/7. 주사위_2480.py
+++ baekjoon/2. 조건문/7. 주사위_2480.py
@@ -6,7 +6,7 @@ if a[0]==a[2]==a[1] :
   print(10000+a[0]*1000)
 
 elif a[0]!=a[1] and a[0]!=a[2] and a[1]!=a[2] :
-    maxValue = a[0]
+    maxValue = a[0]   # 이 방법보다 max(a,b,c)*100을 하는 게 더 간단함
     for i in range(1, len(a)):
         if maxValue < a[i]:
             maxValue = a[i]
