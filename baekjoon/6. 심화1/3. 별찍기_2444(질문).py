f45c22f15a095ba0c6328fb1ba30d6f5d33e7778
eunsun111 <136873704+eunsun111@users.noreply.github.com>
2023-06-23 21:32:06 +0900

    Update 3. 별찍기_2444(질문).py

--- baekjoon/6. 심화1/3. 별찍기_2444(질문).py
+++ baekjoon/6. 심화1/3. 별찍기_2444(질문).py
@@ -1,11 +1,16 @@
-a=[1,3,5,7,9]
+n=int(input())
+
+a=[i for i in range(1,2*n,2)]
 #for i in range(1,10,2) :
 for i in a :
-    for j in range(4-i//2): #빈칸 수가 4,3,2,1,0 이므로 range()안의 숫자를 4,3,2,1,0이 되게 함
-        print(end=" ") # (" ", end="")와 결과가 같음. 빈칸은 하나만 입력해야 위 range의 개수만큼 출력됨. " ",end=" ")이렇게 쓰면 빈칸은 두배가 됨
+    for j in range(n-i//2-1):  #빈칸 수가 0~n-1 이므로 range()안의 숫자를 n-1부터 0까지 나올 수 있게 함
+        print(end=" ")   # (" ", end="")와 결과가 같음. 빈칸은 하나만 입력해야 위 range의 개수만큼 출력됨. " ",end=" ")이렇게 쓰면 빈칸은 두배가 됨
     print("*"*i)
+  
+a.pop()
+a=list(reversed(a)) #Q : a.reverse()로 하면 none 값이 나옴-> 왜???,
 
-for i in [9,7,5,3,1] : #a.reverse()로 하면 none 값이 나옴-> 왜???, b라는 새로운 리스트로 해도 그럼. (copy해야 하나? 나중에 검색해보기)
-    for j in range(4-i//2):
+for i in a :
+    for j in range(n-i//2-1):
         print(end=" ")  
     print("*"*i)
