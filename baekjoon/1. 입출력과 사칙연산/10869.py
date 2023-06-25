a0f16a921dd46bd99878273b3e1066a05aee5fa4
eunsun111 <136873704+eunsun111@users.noreply.github.com>
2023-06-25 18:17:30 +0900

    Update 10869.py

--- baekjoon/1. 입출력과 사칙연산/10869.py
+++ baekjoon/1. 입출력과 사칙연산/10869.py
@@ -8,5 +8,17 @@ b = data[1]
 print(a+b)
 print(a-b)
 print(a*b)
-print(int(a/b))
-print(a%b
+print(int(a//b))
+print(a%b)
+
+""" 2번째 방법
+a, b=map(int,input().split())
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+
+
+"""
