2a856c9595746e09062774daa43f9a5d8c447d11
eunsun111 <136873704+eunsun111@users.noreply.github.com>
2023-06-28 16:07:32 +0900

    Update 02. 문자열 집합_14425.py

--- baekjoon/14. 집단과 맵/02. 문자열 집합_14425(실패).py
+++ baekjoon/14. 집단과 맵/02. 문자열 집합_14425(실패).py
@@ -3,6 +3,27 @@ input = sys.stdin.readline
 
 n,m=map(int, input().split())  #n은 집합 s의 원소 개수, m은 비교군의 개수
 
+#b는 중복가능하기 때문에 집합으로 풀면 안 됨.
+a=[]
+b=[]
+
+for i in range(n):
+    a.append(input().strip())
+
+for i in range(m):
+    b.append(input().strip()) 
+
+c=0
+for i in b :
+   if i in a :
+      c +=1
+
+print(c)
+
+
+
+
+
 """ 1번
 n_list=[]
 m_list=[]
