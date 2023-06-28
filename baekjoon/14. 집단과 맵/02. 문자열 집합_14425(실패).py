1f84b433a3ebfcee56077256050fea40c7737c62
eunsun111 <136873704+eunsun111@users.noreply.github.com>
2023-06-28 15:09:30 +0900

    Update and rename 02. 문자열 집합_14425.py to 02. 문자열 집합_14425(실패).py

--- baekjoon/14. 집단과 맵/02. 문자열 집합_14425(실패).py
+++ baekjoon/14. 집단과 맵/02. 문자열 집합_14425(실패).py
@@ -2,6 +2,8 @@ import sys
 input = sys.stdin.readline
 
 n,m=map(int, input().split())  #n은 집합 s의 원소 개수, m은 비교군의 개수
+
+""" 1번
 n_list=[]
 m_list=[]
 
@@ -15,4 +17,18 @@ n_list=set(n_list)
 m_list=set(m_list)
 
 c=n_list&m_list
+
 print(len(c))
+"""
+"""2번 : 1번과 같은 접근 방법이지만 혹시나 해서 다른 코드 작성함
+a=set()
+b=set()
+
+for i in range(n):
+    a.add(input().strip())
+
+for i in range(m):
+    b.add(input().strip()) 
+
+
+print(int(len(a&b))) """
