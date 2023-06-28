9ad56c31654fbbe3cd1bee8a6c0f7f454e5c324a
eunsun111 <136873704+eunsun111@users.noreply.github.com>
2023-06-28 16:42:47 +0900

    Update and rename 03. 회사에 있는 사람_7785(실패).py to 03. 회사에 있는 사람_7785.py

--- baekjoon/14. 집단과 맵/03. 회사에 있는 사람_7785.py
+++ baekjoon/14. 집단과 맵/03. 회사에 있는 사람_7785.py
@@ -7,8 +7,18 @@ d=dict()
 
 for i in range(n) :
     a, b = input().split()
+    a=a.strip()
+    b=b.strip()
     d[a]=b
 
+r=[]
 for k, v in d.items() :
     if  v=="enter" :
-        print(k)
+        r.append(k)
+r.sort(reverse=True) 
+
+for i in r :
+    print(i)
+
+
+
