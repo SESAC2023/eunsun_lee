6644623763aa8520e4ded826a20991876cbe00d3
eunsun111 <136873704+eunsun111@users.noreply.github.com>
2023-06-30 20:39:59 +0900

    Create 06. 삼각형(실패).py

---
+++ baekjoon/10. 기하/06. 삼각형(실패).py
@@ -0,0 +1,22 @@
+import sys
+
+sys.setrecursionlimit(int(1e6))
+input = sys.stdin.readline
+
+an=[]
+s=0
+for i in range(3):
+    a=int(input())
+    an.append(a)
+    s+=a
+
+if s==180 :
+    if an[0]==60 and an[1]==60:
+        print('Equilateral')
+    elif an[0]==an[1] or an[0]==an[2] :
+        print('Isosceles')
+    else :
+        print('Scalene')
+
+else :
+    print('Error')
