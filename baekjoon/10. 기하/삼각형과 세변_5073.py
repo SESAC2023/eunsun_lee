51bc2d6f86ca3dd7bf2185719db580c65689ac49
eunsun111 <136873704+eunsun111@users.noreply.github.com>
2023-06-30 20:52:40 +0900

    Create 삼각형과 세변_5073.py

---
+++ baekjoon/10. 기하/삼각형과 세변_5073.py
@@ -0,0 +1,18 @@
+import sys
+
+sys.setrecursionlimit(int(1e6))
+input = sys.stdin.readline
+
+while  True:
+    a,b,c=map(int, input().split())
+    if a==0 and b==0 and c==0 :
+        break
+    elif max([a,b,c])>=sum([a,b,c])-max([a,b,c]):
+        print('Invalid')
+    elif a==b and b==c :
+        print('Equilateral')
+    elif a==b or b==c or a==c :
+        print('Isosceles')
+    elif a!=b or b!=c or a!=c :
+        print('Scalene')      
+  
