c5d6fee75a3de9995732b5fc720eb8dcc3e6b1fc
eunsun111 <136873704+eunsun111@users.noreply.github.com>
2023-06-30 19:44:39 +0900

    Update 06. 소인수 분해_11653.py

--- baekjoon/09. 약수,배수,소수/06. 소인수 분해_11653(시간초과).py
+++ baekjoon/09. 약수,배수,소수/06. 소인수 분해_11653(시간초과).py
@@ -1,4 +1,38 @@
 import sys
+import math
+sys.setrecursionlimit(int(1e6))
+input = sys.stdin.readline
+
+n=int(input())  
+
+temp=[] #약수
+x=[] #소수 리스트
+s=0
+
+for i in range(1, n//2):
+    for j in range(1,i+1):
+        if i%j==0 :
+            temp.append(j)#약수
+    if len(temp)==2 :
+        s+=i
+        x.append(i) #소수
+    temp.clear()
+  
+#x.sort()
+print(x)
+
+d=[]
+
+for i in x :
+    while n%i == 0 :
+        n=n//i
+        d.append(i)
+        
+for i in d :
+    print(i, end=' ')
+
+
+"""import sys
 input = sys.stdin.readline
 
 n=int(input())
@@ -17,5 +51,5 @@ for i in b :
     while n%i!=1 :
         c.append(n%i)
 
-print(c)
+print(c)"""
     
