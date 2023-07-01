dbfa23eb9d9a5dc91998b8104fda36f32abf2a71
eunsun111 <136873704+eunsun111@users.noreply.github.com>
2023-07-01 12:01:34 +0900

    Create 01.  수 정렬하기_2750.py

---
+++ baekjoon/12. 정렬/01.  수 정렬하기_2750.py
@@ -0,0 +1,13 @@
+import sys
+
+sys.setrecursionlimit(int(1e6))
+input = sys.stdin.readline
+
+n=int(input())
+b=[]
+for i in range(n) :
+    a=int(input())
+    b.append(a)
+b.sort()
+for i in b :
+    print(i)
