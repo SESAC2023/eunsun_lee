109fe03b9393cb46597ac90956d0bafbb4665442
eunsun111 <136873704+eunsun111@users.noreply.github.com>
2023-07-02 23:16:10 +0900

    Update 02. 회의실 배정_1931(보류).py

--- baekjoon/25. 그리디 알고리즘/02. 회의실 배정_1931(보류).py
+++ baekjoon/25. 그리디 알고리즘/02. 회의실 배정_1931(보류).py
@@ -23,5 +23,19 @@ print(array)
 for i in range(n+1) :
     for j in range(n+1) :
         array[i][0]>=array[j][3]
-        
+
+
+"""from itertools import combinations
+import sys
+
+sys.setrecursionlimit(int(1e6))
+input = sys.stdin.readline
+
+n=int(input())
+num=[i for i in range(1, n+1)]
+
+array=[]
+for i in range(n) :
+    (a,b)=map(int, input().spplit())
+    dict((a,b) =b-a)"""
     
