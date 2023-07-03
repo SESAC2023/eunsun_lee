365ba1a8bd5381d466030755c477b3f4c09a74fb
eunsun111 <136873704+eunsun111@users.noreply.github.com>
2023-07-03 17:49:52 +0900

    Update 09. 미로 탐색_2178(실패).py

--- baekjoon/31. 그래프와 순회/09. 미로 탐색_2178.py
+++ baekjoon/31. 그래프와 순회/09. 미로 탐색_2178.py
@@ -5,43 +5,40 @@ sys.setrecursionlimit(int(1e6))
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-
 array=[]
+
 for i in range(n) :
-    a=input().strip() 
+    a=input().strip()
     temp=[]
-    for i in a :
+    for i in a:
         temp.append(int(i))
+
     array.append(temp)
 
-visited=[[0]*(m)]*(n)
-order=[[0]*m]*(n)
-#print(array)
-#print(visited)
-
-# 상, 하, 좌, 우 정의 
-
-dx = (0, 0, -1, 1)
-dy = (-1, 1, 0, 0)
+visited = [[False]*m]*n
+order = [[0]*m]*n
 
 q=deque()
-visited[0][0] = True
-order[0][0] = 1  
-q.append((0,0)) #출발 노드 넣기
+q.append((0,0))
+visited[0][0] = 1
+order[0][0] = 1
+
+#상 하 좌 우
+dx= [0, 0, -1, 1]
+dy= [-1, 1, 0, 0]
 
 while q :
-    x = q.popleft()
-
-    for i in range(4) :#현재 위치에서 네방향 모두 탐색
-        nx = x +dx[i] 
-        ny = y +dy[i]
-        if nx<0 or ny<0 or nx>=n or ny>=m :
-            continue
-        if array[nx][ny] ==0 :
-            continue
-        if visitied[nx][ny] == 0 :
-            visitied[nx][ny] == 1
-            q.append((nx,ny))
-            order[nx][ny]=order[x][y]+1
+  x, y = q.popleft()
+  for i in range(4) :
+      nx = x + dx[i]
+      ny = y + dy[i]
+      if nx<0 or ny <0 or nx>=n or ny >=m :
+          continue
+      if array[nx][ny] ==0:
+          continue
+      if visited[nx][ny] == False :
+          q.append((nx, ny))
+          visited[nx][ny] == True
+          order[nx][ny] = order[x][y] +1
 
 print(order[n-1][m-1])
