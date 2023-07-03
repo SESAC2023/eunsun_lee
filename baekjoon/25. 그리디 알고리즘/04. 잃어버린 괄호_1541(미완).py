6a47ba178b20b6fb9169cbe04527f1b162d85cc2
eunsun111 <136873704+eunsun111@users.noreply.github.com>
2023-07-03 15:53:57 +0900

    Update and rename 04. 잃어버린 괄호_1541.py to 04. 잃어버린 괄호_1541(미완).py

---
+++ baekjoon/25. 그리디 알고리즘/04. 잃어버린 괄호_1541(미완).py
@@ -0,0 +1,38 @@
+import sys
+
+sys.setrecursionlimit(int(1e6))
+input = sys.stdin.readline
+
+n=input().strip()
+#n='('+n+')'
+n=list(n.split('-'))
+
+a=[]    #더하기로 연결되어 있는 것들을 합하기 위해 분리
+for i in n :
+    p=list(n.split('+'))  #오류 발생
+    a.append(p)
+
+      
+b=[] #합한 값들
+for i in a :
+    x=sum(i)
+    b.append(x)
+  
+s=0
+for i in b[1:] :
+    s -= i
+
+s=s+b[0]
+    
+print(s)
+""" 괄호 식 표현 코드.  문자열이기 떄문에 계산 불가.
+a=""
+
+for i in n[0:-1] :
+    a=a+i+')-('
+
+a=a+n[-1]
+print(a)
+
+#c=n.count("(")-n.count(')')
+#n.append(")"*c)   #모자른 괄호 닫기"""
