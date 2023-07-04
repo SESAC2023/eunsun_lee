68d18a5400af9e2f084b5c681e9665d45158a24e
eunsun111 <136873704+eunsun111@users.noreply.github.com>
2023-07-04 18:29:17 +0900

    Update 06. 스도쿠_2580(미완).py

--- baekjoon/22. 백트래킹/06. 스도쿠_2580(미완).py
+++ baekjoon/22. 백트래킹/06. 스도쿠_2580(미완).py
@@ -14,34 +14,47 @@ print(n)
 row_s=0
 row=[]
 
-for i in n :
-        if i.count(0) ==1 :
-            i[i.index(0)]= 45-sum(i)
+array = [[0]*9 for i in range(9)] #가능한 수 저장하기.
 
-for j in n :
+for i in range(9) : #가로 
+
+    if n[i].count(0) ==1 :
+        n[i][n[i].index(0)]= 45-sum(i)
+    number=[ i for i in range(1,10)]
+    for j in range(9) : #세로
+        if n[i][j] != 0 :
+            n[i].remove(array[i][j])
+    array[i][j].append(number)   
+             
+        
+print(n)
+print(array)
+
+for i in range(9) : #세로
         row_s=0
-        row=[]
-        for x in range(9) :
-            row_s+=j[x]
-            row.append(j)
+        row_s_list = []
+        zero_c = 0
+        for j in range(9) : #가로
+            row_s+=array[j][i]
+            if array[j][i] == 0 :
+                zero_c+=1
             if row_s==45 :
                 pass
-            elif row.count(0)==1 :
-                j[j.index(0)] = (45-sum(row))
+            elif row!=45 and zero_c==1 :
+                array[j][i] = (45-row_s)
 
 
 print(n)
 for i in range(0,9,3) :
-
     for j in range(0,9,3) :
 
-        s=sum(n[i][j:j+3])+sum(n[i+1][j:j+3])+sum(n[i+2][j:j+3])
-        c=n[i][j:j+3].count(0)+n[i+1][j:j+3].count(0)+n[i+2][j:j+3].count(0)
+        s=sum(n[i][j:j+3])+sum(n[i+1][j:j+3])+sum(n[i+2][j:j+3]) # 3*3 -> 9개 숫자의 합
+        c=n[i][j:j+3].count(0)+n[i+1][j:j+3].count(0)+n[i+2][j:j+3].count(0) # 9개 중 0이 몇 개인지
         if c==1 :
             if 0 in [i+1][i:i+3] :
-                n[n[i][i:i+3].index(0)]=(45-s)
+                n[i][n[i][i:i+3].index(0)+i]=(45-s)
             if 0 in [i+1][i:i+3] :
-                n[n[i][i:i+3].index(0)]=(45-s)
+                n[n[i][i:i+3].index(0)+i]=(45-s)
             if 0 in [i+1][i:i+3] :
-                n[n[i][i:i+3].index(0)]=(45-s)
+                n[n[i][i:i+3].index(0)+i]=(45-s)
 print(n)
