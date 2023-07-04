e4c77de1c3ccd960c23a3d56707d832e6b9dfbd7
eunsun111 <136873704+eunsun111@users.noreply.github.com>
2023-07-04 10:50:29 +0900

    Update 04. 군형잡힌 세상_4949(실패).py

--- baekjoon/19. 스택/04. 군형잡힌 세상_4949(실패).py
+++ baekjoon/19. 스택/04. 군형잡힌 세상_4949(실패).py
@@ -1,8 +1,41 @@
+
 import sys
 
 sys.setrecursionlimit(int(1e6))
 input = sys.stdin.readline
 
+array = []
+while True:
+  y = input()
+  if y == ".":
+      break
+  for i in y:
+      if i == '(' or '{' or '[' :
+          array.append(i)
+      if i == ')' or '}' or ']':
+          if array == [] :
+              print('no')
+              break
+          else :  
+              if i==")" and array[-1]=='(' : 
+                  array.pop()
+              elif i=="}" and array[-1]=='{' : 
+                  array.pop()
+              elif i=="]" and array[-1]=='[' : 
+                  array.pop()
+              else :
+                   print('no')
+                   break
+          
+    
+
+  if len(array)==0:
+      print('yes')
+  else:
+      print('no')
+
+
+""" 아래의 코드는 (([))]  와 같은 사례를 구분하지 못함.
 while True:
   s = 0
   m = 0
@@ -37,4 +70,4 @@ while True:
   elif s == 0 and m == 0 and l == 0:
     print('yes')
   else:
-    print('no')
+    print('no') """
