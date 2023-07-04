adb58732b81cb75ad3330a7c73ffa2fe668d3fa7
eunsun111 <136873704+eunsun111@users.noreply.github.com>
2023-07-04 11:33:49 +0900

    Update 03. 괄호_9012(실패).py

--- baekjoon/19. 스택/03. 괄호_9012(실패).py
+++ baekjoon/19. 스택/03. 괄호_9012(실패).py
@@ -3,6 +3,35 @@ import sys
 sys.setrecursionlimit(int(1e6))
 input = sys.stdin.readline
 
+k= int(input())
+array=[]
+
+for j in range(k) :
+  a = input().strip()
+  array=[]
+  for i in a:
+      print(array)
+      if array == [] and i ==")" :
+          print('NO')
+          array.append('1')
+          break
+      if i == '(' :
+          array.append(i)
+      if i == ')' :
+          array.pop()
+          print(array)
+  if len(array)==0:
+      print('YES')
+  else:
+      print('NO')
+
+
+"""
+import sys
+
+sys.setrecursionlimit(int(1e6))
+input = sys.stdin.readline
+
 k = int(input())
 array = []
 s=0
@@ -22,4 +51,4 @@ for i in range(k) :
     else :
         array.append('NO')
 for i in array :
-    print(i)
+    print(i) """
