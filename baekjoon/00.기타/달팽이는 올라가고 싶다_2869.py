2f24cd1ac89468020230191249a23643dbd9d293
eunsun111 <136873704+eunsun111@users.noreply.github.com>
2023-07-16 21:26:22 +0900

    Create 달팽이는 올라가고 싶다_2869.py

---
+++ baekjoon/00.기타/달팽이는 올라가고 싶다_2869.py
@@ -0,0 +1,37 @@
+"""재귀 런타임 오류
+
+a, b, v = map(int, input().split()) #a 낮시간에 올라가는 길이, b는 밤에 내려오는 길이, v는 나무 길이
+
+v1=v-a #전날까지 도달해야 하는 최소 높이
+v2 = v1 % (a-b)
+v3 = v1 // (a-b)
+
+days = 0
+hight = 0
+def hi(d, n, h) : #낮, 밤, 높이
+    global days
+    global hight
+    days += 1
+    hight += d
+    if hight < h :
+        hight -= n
+        return hi(d, n, h)
+    else :
+        return days
+
+print(hi(a, b, v))
+"""
+
+
+a, b, v = map(int, input().split()) #a 낮시간에 올라가는 길이, b는 밤에 내려오는 길이, v는 나무 길이
+
+v1=v-a #전날까지 도달해야 하는 최소 높이
+v2 = v1 % (a-b) #항상 a보다 작을 
+v3 = v1 // (a-b)
+
+if a == v :
+    print(1)
+elif v //(a-b) :
+    print(2)
+else :
+    print(round(v1//(a-b)) +1)
