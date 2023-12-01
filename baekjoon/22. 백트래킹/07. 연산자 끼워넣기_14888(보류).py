edb209a926aa9f5ca2c54c08a0b53d46bcd16a31
eunsun111 <136873704+eunsun111@users.noreply.github.com>
2023-12-01 16:43:49 +0900

    Update 07. 연산자 끼워넣기_14888(보류).py

--- baekjoon/22. 백트래킹/07. 연산자 끼워넣기_14888(보류).py
+++ baekjoon/22. 백트래킹/07. 연산자 끼워넣기_14888(보류).py
@@ -9,10 +9,22 @@ num=list(map(int, input().split()))
 
 
 p,m,t,d=map(int,input().split())  #차례대로 덧셈(+)의 개수, 뺄셈(-)의 개수, 곱셈(×)의 개수, 나눗셈(÷)의 개수
+l = []
 
-array = ['+'*p, '-'*m, '*'*t, '//'*d]
+for i in range(p) :
+    l.append("+")
+for i in range(m) :
+    l.append("-")
+for i in range(t) :
+    l.append("*")
+for i in range(d) :
+    l.append("//")
+
+array = permutations(l)
+array = list(set(array))
 for i in array :
-    if i=="" :
-        array.remove("")
-array = permutations(array)
+    print(i)
 
+str = ""
+for i in range(n) :
+    num[i]+
