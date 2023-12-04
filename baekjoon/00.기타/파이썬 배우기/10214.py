1430a17c48046553f860787acb45b668ddeba12e
eunsun111 <136873704+eunsun111@users.noreply.github.com>
2023-12-04 14:40:55 +0900

    Create 10214.py

---
+++ baekjoon/00.기타/파이썬 배우기/10214.py
@@ -0,0 +1,20 @@
+import sys
+
+sys.setrecursionlimit(int(1e6))
+input = sys.stdin.readline
+
+tc = int(input())
+
+for i in range(tc) :
+    y = 0
+    k = 0
+    for j in range(9) :
+        a, b = map(int, input().split())
+        y += a
+        k += b
+    if y > k :
+        print("Yonsei")
+    elif y < k :
+        print("Korea")
+    else :
+        print("Draw")
